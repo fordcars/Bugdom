fd7e927428b864207f21d4652d9cbebbd9478728
Carl Hewett <carl.hewett@polymtl.ca>
2025-01-25 02:55:30 -0500

    Fixed shadows

--- src/Headers/camera.h
+++ src/Headers/camera.h
@@ -3,11 +3,7 @@
 //
 
 #define	HITHER_DISTANCE	20.0f
-#ifdef __3DS__
-    #define	YON_DISTANCE	2000.0f
-#else
-    #define	YON_DISTANCE	2500.0f
-#endif
+#define	YON_DISTANCE	2500.0f
 
 void InitCamera(void);
 void UpdateCamera(void);
